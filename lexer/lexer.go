fcead6f56c904935e6c9e8cba51d8555b3486aa4
Achintya <achintya22052000@.com>
2024-10-12 17:53:14 +0530

    feat: implemented lexer with test

---
+++ lexer/lexer.go
@@ -0,0 +1,148 @@
+package lexer
+
+import (
+	"fmt"
+	"parser/constants"
+	"unicode"
+)
+
+type Lexerer interface {
+	NewToken() constants.Token
+	readNumber() string
+	readIdentifer() string
+	peakChar() byte
+	skipWhitespace()
+}
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+	line         int
+}
+
+func NewLexer(input string) Lexerer {
+	l := &Lexer{input: input, line: 1}
+	l.readChar()
+	return l
+}
+
+// readChar reads the next character in the input string and advances the position in the input string.
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) ToSring() string {
+	return fmt.Sprintf("input: %s, position: %d, readPosition: %d, ch: %c, line: %d", l.input, l.position, l.readPosition, l.ch, l.line)
+}
+
+func (l *Lexer) NewToken() constants.Token {
+	var tok constants.Token
+	skipReadChar := false
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		if l.peakChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = constants.Token{Type: constants.TOKEN_DOUBLE_EQUAL, Lexeme: string(ch) + string(l.ch)}
+		} else {
+			tok = constants.Token{Type: constants.TOKEN_EQUAL, Lexeme: string(l.ch)}
+		}
+	case '<':
+		if l.peakChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = constants.Token{Type: constants.TOKEN_NOT_EQUAL, Lexeme: string(ch) + string(l.ch)}
+		}
+	case '!':
+		tok = constants.Token{Type: constants.TOKEN_NOT, Lexeme: string(l.ch)}
+	case '+':
+		tok = constants.Token{Type: constants.TOKEN_PLUS, Lexeme: string(l.ch)}
+	case '-':
+		tok = constants.Token{Type: constants.TOKEN_MINUS, Lexeme: string(l.ch)}
+	case '*':
+		tok = constants.Token{Type: constants.TOKEN_MULTIPLY, Lexeme: string(l.ch)}
+	case '/':
+		tok = constants.Token{Type: constants.TOKEN_DIVIDE, Lexeme: string(l.ch)}
+	case '(':
+		tok = constants.Token{Type: constants.TOKEN_LEFT_PAREN, Lexeme: string(l.ch)}
+	case ')':
+		tok = constants.Token{Type: constants.TOKEN_RIGHT_PAREN, Lexeme: string(l.ch)}
+	case 0:
+		tok.Lexeme = ""
+		tok.Type = constants.TOKEN_EOF
+		tok.Line = l.line
+	default:
+		if unicode.IsDigit(rune(l.ch)) {
+			tok = constants.Token{
+				Type:   constants.TOKEN_NUMBER,
+				Lexeme: l.readNumber(),
+			}
+			skipReadChar = true
+		} else if unicode.IsLetter(rune(l.ch)) {
+			tok = constants.Token{
+				Type:   constants.TOKEN_IDENTIFIER,
+				Lexeme: l.readIdentifer(),
+			}
+			skipReadChar = true
+		} else {
+			tok = constants.Token{
+				Type:   constants.TOKEN_EOF,
+				Lexeme: string(l.ch),
+			}
+		}
+	}
+
+	tok.Line = l.line
+	if !skipReadChar {
+		l.readChar()
+	}
+	return tok
+}
+
+func (l *Lexer) readNumber() string {
+	currentPosition := l.position
+
+	for unicode.IsDigit(rune(l.ch)) {
+		l.readChar()
+	}
+
+	return l.input[currentPosition:l.position]
+
+}
+func (l *Lexer) readIdentifer() string {
+	currentPosition := l.position
+
+	for unicode.IsLetter(rune(l.ch)) {
+		l.readChar()
+	}
+
+	return l.input[currentPosition:l.position]
+}
+
+func (l *Lexer) peakChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+
+	return l.input[l.readPosition]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		if l.ch == '\n' {
+			l.line++
+		}
+		l.readChar()
+	}
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,76 @@
+package lexer
+
+import (
+	"parser/constants"
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []constants.Token
+	}{
+		{
+			input: "+-*/()",
+			expected: []constants.Token{
+				{Type: constants.TOKEN_PLUS, Lexeme: "+", Line: 1},
+				{Type: constants.TOKEN_MINUS, Lexeme: "-", Line: 1},
+				{Type: constants.TOKEN_MULTIPLY, Lexeme: "*", Line: 1},
+				{Type: constants.TOKEN_DIVIDE, Lexeme: "/", Line: 1},
+				{Type: constants.TOKEN_LEFT_PAREN, Lexeme: "(", Line: 1},
+				{Type: constants.TOKEN_RIGHT_PAREN, Lexeme: ")", Line: 1},
+				{Type: constants.TOKEN_EOF, Lexeme: "", Line: 1},
+			},
+		},
+		{
+			input: "1 + 25",
+			expected: []constants.Token{
+				{Type: constants.TOKEN_NUMBER, Lexeme: "1", Line: 1},
+				{Type: constants.TOKEN_PLUS, Lexeme: "+", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "25", Line: 1},
+				{Type: constants.TOKEN_EOF, Lexeme: "", Line: 1},
+			},
+		},
+		{
+			input: "0 / 22",
+			expected: []constants.Token{
+				{Type: constants.TOKEN_NUMBER, Lexeme: "0", Line: 1},
+				{Type: constants.TOKEN_DIVIDE, Lexeme: "/", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "22", Line: 1},
+				{Type: constants.TOKEN_EOF, Lexeme: "", Line: 1},
+			},
+		},
+		{
+			input: "(1 + 2 * 3) + (x - y / 2)",
+			expected: []constants.Token{
+				{Type: constants.TOKEN_LEFT_PAREN, Lexeme: "(", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "1", Line: 1},
+				{Type: constants.TOKEN_PLUS, Lexeme: "+", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "2", Line: 1},
+				{Type: constants.TOKEN_MULTIPLY, Lexeme: "*", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "3", Line: 1},
+				{Type: constants.TOKEN_RIGHT_PAREN, Lexeme: ")", Line: 1},
+				{Type: constants.TOKEN_PLUS, Lexeme: "+", Line: 1},
+				{Type: constants.TOKEN_LEFT_PAREN, Lexeme: "(", Line: 1},
+				{Type: constants.TOKEN_IDENTIFIER, Lexeme: "x", Line: 1},
+				{Type: constants.TOKEN_MINUS, Lexeme: "-", Line: 1},
+				{Type: constants.TOKEN_IDENTIFIER, Lexeme: "y", Line: 1},
+				{Type: constants.TOKEN_DIVIDE, Lexeme: "/", Line: 1},
+				{Type: constants.TOKEN_NUMBER, Lexeme: "2", Line: 1},
+				{Type: constants.TOKEN_RIGHT_PAREN, Lexeme: ")", Line: 1},
+				{Type: constants.TOKEN_EOF, Lexeme: "", Line: 1},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		l := NewLexer(tt.input)
+		for i, expectedToken := range tt.expected {
+			tok := l.NewToken()
+			// t.Log(tok.ToString()) // Uncomment this line to see the tokens
+			if tok != expectedToken {
+				t.Fatalf("test[%d] - token wrong. expected=%q, got=%q", i+1, expectedToken, tok)
+			}
+		}
+	}
+}
